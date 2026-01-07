719dcf30f3d4b0f661b901243331b6d9f44a535c
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-01-07 19:10:35 +0530

    added data file

---
+++ data.py
@@ -0,0 +1,10 @@
+student_info = {
+    0:{"name":"SURYA",
+        "age":20,
+        "school":"SCORE",
+        "courses":{
+            "python":[0,0],
+            "agile":[0,0]
+            }
+      }
+}
